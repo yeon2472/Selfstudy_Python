3f4f70d41d6b5e361a7591046817c3b85dfa146c
juyeon <juhaa47@naver.com>
2021-10-08 21:15:37 +0900

    문자열2

--- study8_string.py
+++ study8_string.py
@@ -65,3 +65,47 @@ if cnt_list.count(max(cnt_list)) >1 :
 else :
     max_index = cnt_list.index(max(cnt_list))
     print(unique_words[max_index])
+
+
+##Baekjoon_1152번_ 단어의 개수
+# case 1
+word = input().split()
+print(len(word))
+# case 2
+print(len(input().split()))
+
+
+ ##Baekjoon_2908번_상수
+num1, num2 = input().split()
+num1 = int(num1[::-1])
+num2 = int(num2[::-1])
+
+print(num1) if num1 > num2 else print(num2)
+
+##Baekjoon_5622번_다이얼
+alpabet_list = ['ABC','DEF','GHI','JKL','MNO','PQRS','TUV','WXYZ']
+word = input()
+
+time = 0
+for unit in alpabet_list :
+    for i in unit:  # alpabet 리스트에서 각 요소를 꺼내서 한글자씩 분리
+        for x in word :  # 입력받은 문자를 하나씩 분리
+            if i == x :  # 두 알파벳이 같으면
+                time += alpabet_list.index(unit) +3
+print(time)
+
+##Baekjoon_1316번_그룹단어체커
+n = int(input())
+
+group_word = 0
+for _ in range(n):
+    word = input()
+    error = 0
+    for index in range(len(word)-1):  # 인덱스 범위 생성 : 0부터 단어개수 -1까지
+        if word[index] != word[index+1]:  # 연달은 두 문자가 다른 때,
+            new_word = word[index+1:]  # 현재글자 이후 문자열을 새로운 단어로 생성
+            if new_word.count(word[index]) > 0:  # 남은 문자열에서 현재글자가 있있다면
+                error += 1  # error에 1씩 증가.
+    if error == 0:
+        group_word += 1  # error가 0이면 그룹단어
+print(group_word)
